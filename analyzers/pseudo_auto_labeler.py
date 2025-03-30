211647c190cfc1745bd079f9969631fb768b3bd3
Dimitrios Haralampopoulos <dimi.haralampopoulos@gmail.com>
2025-03-30 07:23:36 -0400

    Cleaned up repo
    Removed unnecessary files, consolidated code

--- analyzers/pseudo_auto_labeler.py
+++ analyzers/pseudo_auto_labeler.py
@@ -27,3 +27,9 @@ results = {"id": [d["id"] for d in results],
            }
 
 json.dump(results, open("reddit_sentiment_analysis.json", "w+"))
+infodf = pl.from_dict(json.load(open("../data/reddit_ice_info.json", "r+")))
+sentimentdf = pl.from_dict(json.load(open("../data/reddit_sentiment_analysis.json", "r+")))
+
+new_df = infodf.join(sentimentdf, on="id")
+new_df[3, "place"] = "Neptune, New Brunswick, Paulsboro, Newark, Lakewood, Belmar"
+json.dump(new_df.to_dict(as_series=False), open("../data/reddit_final_data.json", "w+"))
